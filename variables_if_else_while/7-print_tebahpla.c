fa6337185d8fa06a8eb081858ca2d14af88b11b3
root <Nada.Ghannam19@gmail.com>
2025-08-26 14:38:44 +0000

    add

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * My function: loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void){
+for( int a=122;a<=97;a--)
+putchar(a);
+putchar('\n');
+}
+
