63996ea9b2369c60daec101e7980b3a231f4fbbb
root <Nada.Ghannam19@gmail.com>
2025-09-01 17:00:49 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,49 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - convert a string to an integer
- * @s: string to convert
+ * @s: input string
  *
  * Return: integer value
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    unsigned int num = 0;
-    int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int digit;
+	int found = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            found_digit = 1;
-            num = (num * 10) + (s[i] - '0');
-        }
-        else if (found_digit)
-            break;
-        i++;
-    }
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		i++;
+	}
 
-    return (sign * (int)num);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		found = 1;
+
+		if (result < (INT_MIN + digit) / 10)
+			return (sign == 1 ? INT_MAX : INT_MIN);
+
+		result = result * 10 - digit;
+		i++;
+	}
+
+	if (!found)
+		return (0);
+
+	if (sign == 1)
+	{
+		if (result == INT_MIN)
+			return (INT_MIN);
+		return (-result);
+	}
+	return (result);
 }
