4c8630ce5554ab87fa1546531db68bbf42aa55be
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-03 00:02:36 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,41 @@
 #include "main.h"
-#include <limits.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: the string to convert
+ * _atoi - Converts a string to an integer
+ * @s: String to be converted
  *
- * Return: the integer value of the string, or 0 if none
+ * Return: The integer value of the string, or 0 if no valid number is found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1;
+	int sign = 1;
 	int result = 0;
-	int found_digit = 0;
+	int i = 0;
+	int digit;
 
-	while (s[i] != '\0')
+	/* Skip non-digit characters and process signs */
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			int digit = s[i] - '0';
-			found_digit = 1;
-
-			/* check overflow before multiplying/adding */
-			if (sign == 1 && (result > (INT_MAX - digit) / 10))
-				return (INT_MAX);
-			if (sign == -1 && (result > (INT_MIN + digit) / -10))
-				return (INT_MIN);
-
-			result = result * 10 + digit;
-		}
-		else if (found_digit)
-			break;
+		else if (s[i] == '+')
+			sign *= 1;
 		i++;
 	}
 
-	return (result * sign);
+	/* Convert digits to integer with overflow check */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		if (result > (2147483647 - digit) / 10)
+		{
+			if (sign == 1)
+				return (2147483647);
+			return (-2147483648);
+		}
+		result = result * 10 + digit;
+		i++;
+	}
+
+	return (sign * result);
 }
