3454d2bdbb255110050919367d792e8e8aad053a
root <Nada.Ghannam19@gmail.com>
2025-09-02 15:36:17 +0000

    Add

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- 
-puts_half - prints the second half of a string
-@str: pointer to the string*
-Return: void*/
+ * puts_half - prints half of a string
+ * @str: input string
+ */
 void puts_half(char *str)
 {
     int len = 0, i;
