057f7f32f953fca7cb7088e99f40e48d0f418674
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-05 04:23:43 +0300

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -5,30 +5,33 @@
  *
  * Description: This function converts the first character of each
  * word in the string @s to uppercase. Words are separated by
- * the following separators:
- * space, tabulation, new line, ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'
+ * the following separators: space, tabulation, new line,
+ * ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'.
  *
  * Return: pointer to the modified string
  */
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j;
 	char separators[] = " \t\n,;.!?\"(){}";
 
 	if (s[0] >= 'a' && s[0] <= 'z')
+	{
 		s[0] = s[0] - 32;
-	
+	}
+
 	while (s[i] != '\0')
 	{
-		int j = 0;
-		
+		j = 0;
 		while (separators[j] != '\0')
 		{
 			if (s[i] == separators[j])
 			{
-				
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
 					s[i + 1] = s[i + 1] - 32;
+				}
 				break;
 			}
 			j++;
