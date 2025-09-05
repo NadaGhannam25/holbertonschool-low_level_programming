c9470a7679fd0fcd01b62835bed5d243a06fd851
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-05 04:13:57 +0300

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,38 @@
+3. strcmp
+mandatory
+Write a function that compares two strings.
+
+Prototype: int _strcmp(char *s1, char *s2);
+Your function should work exactly like strcmp
+FYI: The standard library provides a similar function: strcmp. Run man strcmp to learn more.
+
+julien@ubuntu:~/$ cat 3-main.c
+#include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
- * _strcmp -  compares two strings
+ * main - check the code
  *
- * @s1: pointer to an char
- * @s2: pointer to an char
- *
- * Return: int
+ * Return: Always 0.
  */
-
-int _strcmp(char *s1, char *s2)
+int main(void)
 {
-	int i = 0;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-	while ((s1[i] != '\0') || (s2[i] != '\0'))
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	return (0);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
+julien@ubuntu:~/$ ./3-strcmp 
+-15
+15
+0
+julien@ubuntu:~/$ 
+Repo:
+
+GitHub repository: holbertonschool-low_level_programming
+Directory: pointers_arrays_strings
+File: 3-strcmp.c
