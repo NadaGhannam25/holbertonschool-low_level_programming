183cb067c542a419a405731f89d762c3556f83d1
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-05 04:08:52 +0300

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,36 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-
 /**
  * _strcat - concatenates two strings
+ * @dest: destination string (must have enough space)
+ * @src: source string to append
  *
- * @dest: pointer to an char
- * @src: pointer to an char
+ * Description: This function appends the @src string to
+ * the @dest string, overwriting the terminating null byte
+ * (\0) at the end of @dest, and then adds a terminating
+ * null byte at the end.
  *
- * Return: dest (Success)
+ * Return: pointer to resulting string @dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i = strlen(src);
-	int k = strlen(dest);
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (j = 0; j < i; j++)
-		dest[k + j] = src[j];
-	for (; j < k; j++)
-		dest[k + j] = '\0';
+	/* find end of dest */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* append src */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0'; /* add null terminator */
+
 	return (dest);
 }
