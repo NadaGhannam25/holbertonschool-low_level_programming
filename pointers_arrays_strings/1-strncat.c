a20f4936179f1be457069dae5fc2846b401fe771
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-05 04:11:07 +0300

    Update 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-
+#include "main.h"
 /**
- * _strncat - concatenates two strings
+ * _strncat - concatenates two strings using at most n bytes from src
+ * @dest: destination string (must have enough space)
+ * @src: source string to append
+ * @n: maximum number of bytes to use from src
  *
- * @dest: pointer to an char
- * @src: pointer to an char
- * @n: int
+ * Description: This function appends the @src string to the @dest string,
+ * using at most @n bytes from @src. If @src contains n or more bytes,
+ * it does not need to be null-terminated. The resulting string is
+ * always null-terminated.
  *
- * Return: dest
+ * Return: pointer to resulting string @dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = strlen(src);
-	int k = strlen(dest);
-	int j;
+	int i = 0;
+	int j = 0;
 
-	for (j = 0; (j < n) & (j < i); j++)
-		dest[k + j] = src[j];
-	for (; j < k; j++)
-		dest[k + j] = '\0';
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
 	return (dest);
 }
