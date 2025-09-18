d28216dbfe2957fdbbbb38ae0981bbf071b6eaf3
root <Nada.Ghannam19@gmail.com>
2025-09-18 19:56:29 +0000

    add

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - concatenates two strings
+ * *string_nconcat - Concatinates two strings
  *
- * @s1: a pointer to a char
- * @s2: a pointer to a char
- * @n: an unsigned integer
+ * @s1: char pointer
  *
- * Return: returned pointer shall point to a newly allocated space in memory
+ * @s2: char pointer
+ *
+ * @n: unsigned int
+ *
+ * Return: Pointer to s1, n bytes of s2
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int j, l = 0;
-	char *t;
+unsigned int i, j, len1 = 0, len2 = 0;
+char *str;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	t = malloc(sizeof(char) * (strlen(s1) + n + 1));
-	if (t == NULL)
-		return (NULL);
-	for (j = 0; j < (strlen(s1) + n); j++)
-	{
-		if (j < strlen(s1))
-			t[j] = s1[j];
-		else
-		{
-			t[j] = s2[l];
-			l++;
-		}
-	}
-	t[j] = '\0';
-	return (t);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
+
+if (n >= len2)
+n = len2;
+
+str = malloc(sizeof(char) * (len1 + n + 1));
+if (str == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+str[i] = s1[i];
+
+for (j = 0; j < n; j++)
+str[i + j] = s2[j];
+
+str[i + j] = '\0';
+
+return (str);
 }
