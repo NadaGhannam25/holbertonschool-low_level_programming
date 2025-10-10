3a9d7d26cde8330ea50095ced79f355dcbb0c90e
root <Nada.Ghannam19@gmail.com>
2025-10-10 04:57:20 +0000

    add

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,48 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: a list
- * @str: a string of character
- * Return: the address of the new element, or NULL if it failed
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Pointer to pointer to the head of the list
+ * @str: String to add to the new node
+ *
+ * Return: Address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *tail;
+    list_t *new_node;
+    list_t *temp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = NULL;
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		tail = *head;
-		while (1)
-		{
-			if (tail->next == NULL)
-			{
-				tail->next = new;
-				break;
-			}
-			tail = tail->next;
-		}
-	}
-	return (new);
+    if (!str)
+        return (NULL);
+
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    temp = *head;
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = new_node;
+
+    return (new_node);
 }
+
