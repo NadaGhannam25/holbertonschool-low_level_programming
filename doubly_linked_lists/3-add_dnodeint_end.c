5cb873e3d12304c9e913bb75e5ce929935b6ec13
root <Nada.Ghannam19@gmail.com>
2025-10-11 08:37:19 +0000

    update

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,15 +1,37 @@
 #include "lists.h"
 
 /**
- * free_dlistint - frees a dlistint_t list
- * @head: a linked list
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: a list
+ * @n: a constante integer
+ * Return: the address of the new element, or NULL if it failed
  */
 
-void free_dlistint(dlistint_t *head)
-{
-	if (head)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n){
+	dlistint_t *new;
+	dlistint_t *tail;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
 	{
-		free_dlistint(head->next);
-		free(head);
+		tail = *head;
+		while (1)
+		{
+			if (tail->next == NULL)
+			{
+				tail->next = new;
+				new->prev = tail;
+				break;
+			}
+			tail = tail->next;
+		}
 	}
+	return (new);
 }
